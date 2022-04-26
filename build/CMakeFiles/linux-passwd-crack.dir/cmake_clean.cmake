f952682bc47cf86a3480c573e21fc18f738a69a2
Exit-Zer0 <exitzero1904@gmail.com>
2022-04-26 12:50:35 +0800

    sh1t

---
+++ build/CMakeFiles/linux-passwd-crack.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linux-passwd-crack.dir/main.cpp.o"
+  "linux-passwd-crack.pdb"
+  "linux-passwd-crack"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/linux-passwd-crack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/桌面/linux-passwd-crack
+# Build directory: /home/student/桌面/linux-passwd-crack/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
